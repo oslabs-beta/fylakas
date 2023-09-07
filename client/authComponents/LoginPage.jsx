26d20dad02bfab1012dd8ab289dd3953ab98b5d4
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-09-07 13:59:14 -0400

    Set up mock frontend login/signup functionality

--- client/authComponents/LoginPage.jsx
+++ client/authComponents/LoginPage.jsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import LoginBox from './LoginBox.jsx';
 import SignupBox from './SignupBox.jsx';
 
-const LoginPage = (props) => {
+const LoginPage = ({logIn}) => {
   const [accountExists, setAccountExists] = useState(true);
 
   const page = accountExists ? 
-  <LoginBox routeToSignupPage = {() => setAccountExists(false)}/> : 
-  <SignupBox routeToLoginPage = {() => setAccountExists(true)}/>; 
+  <LoginBox routeToSignupPage = {() => setAccountExists(false)} logIn = {logIn}/> : 
+  <SignupBox routeToLoginPage = {() => setAccountExists(true)} logIn = {logIn}/>; 
 
   return (
     <div>
