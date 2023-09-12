c53a1a214db951be7aa273153604aa004c3ef7b7
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-09-12 15:56:50 -0400

    Added functionality to logout button on frontend; added logout route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,7 +25,7 @@ authController.handleUserDetails = async (req, res, next) => {
     if (res.locals.valid) res.locals.hpassword = await bcrypt.hash(password, WORKFACTOR);
     next();
   } catch (err) {
-    next({log: err, message: {err: 'Error occured handling user details.'}});
+    next({log: err, message: {err: 'Error occurred handling user details.'}});
   }
 }
 
@@ -48,7 +48,7 @@ authController.login = async (req, res, next) => {
     }
     next();
   } catch (err) {
-    next({log: err, message: {err: 'Error occured in login.'}});
+    next({log: err, message: {err: 'Error occurred in login.'}});
   }
 }
 
@@ -80,7 +80,7 @@ authController.signup = async (req, res, next) => {
     }
     next();
   } catch (err) {
-    next({log: err, message: {err: 'Error occured in signup.'}});
+    next({log: err, message: {err: 'Error occurred in signup.'}});
   }
 }
 
@@ -104,7 +104,7 @@ authController.startSession = async (req, res, next) => {
     }
     next();
   } catch (err) {
-    next({log: err, message: {err: 'Error occured starting user session.'}});
+    next({log: err, message: {err: 'Error occurred starting user session.'}});
   }
 }
 
@@ -130,7 +130,16 @@ authController.isLoggedIn = async (req, res, next) => {
     }
     next();
   } catch (err) {
-    next({log: err, message: {err: 'Error occured checking user credentials.'}})
+    next({log: err, message: {err: 'Error occurred checking user credentials.'}})
+  }
+}
+
+authController.logout = async (req, res, next) => {
+  try {
+    res.clearCookie('jwt');
+    return next();
+  } catch (err) {
+    next({log: err, message: {err: 'Error occurred while logging out.'}})
   }
 }
 
