1a4c29d92e486acdf08fd776ffcf30ca0b56b2c5
Bogdana Oliynyk <bogdanao003@gmail.com>
2023-09-20 14:34:09 -0500

    added a examples in pop up menus for metic type, context, and group

--- client/dashboardComponents/MetricModal.jsx
+++ client/dashboardComponents/MetricModal.jsx
@@ -1,3 +1,4 @@
+//group by will pop up, as well as more parameters will next to context if a certain metric type is selected.
 return (
     <div className="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1">
       <div className="modal-dialog">
@@ -11,19 +12,43 @@ return (
                 <div className="form-floating mb-3">
                   Metric Type:
                   <ul className="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px" data-bs-theme="light">
-
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">CPU Usage</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Memory Usage by Container</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Memory Available in Nodes</a>
+                    </li>
                   </ul>
                 </div>
                 <div className="form-floating mb-3">
                   Context:
                   <ul className="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px" data-bs-theme="light">
-
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Cluster</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Namespace</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Node</a>
+                    </li>
                   </ul>
                 </div>
                 <div className="form-floating mb-3">
                   Group By:
                   <ul className="dropdown-menu position-static d-grid gap-1 p-2 rounded-3 mx-0 shadow w-220px" data-bs-theme="light">
-
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">View All</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Containers</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item rounded-2 active" href="#">Nodes</a>
+                    </li>
                   </ul>
                 </div>
                 <div>
