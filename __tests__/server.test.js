5171aba7f46d5071005f2c62dd9a46d19fea39b5
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-09-20 12:06:51 -0400

    created simple test file. adjusted app path.

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,1 +1,20 @@
+const request = require('supertest');
+const app = require('../server/server');
 
+describe('Server Routes', () => {
+  describe('GET /', () => {
+    it('should respond with the index.html file', async () => {
+      const response = await request(app).get('/');
+      expect(response.statusCode).toBe(200);
+      expect(response.type).toBe('text/html');
+    });
+  });
+
+  describe('Unknown Routes', () => {
+    it('should respond with a 404 status and message', async () => {
+      const response = await request(app).get('/unknownpath');
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toBe("This is not the page you're looking for...");
+    });
+  });
+});
