d298c03593f8d97102e4d5927b59bc2421f060bf
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-09-21 11:32:14 -0400

    1st apporach to login tests.

--- __tests__/AuthRouter.test.js
+++ __tests__/AuthRouter.test.js
@@ -1,3 +1,21 @@
 const request = require('supertest');
 const express = require('express');
 const authRouter = require('../server/routers/authRouter'); // adjust path to your router file
+
+const app = express();
+app.use(express.json());
+app.use('/auth', authRouter);
+
+describe('Auth Routes', () => {
+  // Test for /login
+  describe('POST /auth/login', () => {
+    it('should login with valid credentials', async () => {
+      const response = await request(app)
+        .post('/auth/login')
+        .send({ username: 'testuser', password: 'testpassword' }); // Adjust with actual credentials
+      expect(response.statusCode).toBe(200);
+      expect(response.body.profile).toBeDefined();
+      // Other assertions as necessary...
+    });
+  });
+});
