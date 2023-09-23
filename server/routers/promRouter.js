aad707ab06018e24cb844c8bbf1eedddaf94ada5
Katherine Fry <katgfry@me.com>
2023-09-22 20:04:19 -0400

    connected prometheus and implemented PromController.cpuUsageByContainer middleware

--- server/routers/promRouter.js
+++ server/routers/promRouter.js
@@ -4,11 +4,11 @@ const Promcontroller = require('../controllers/promController.js');
 const promRouter = express.Router();
 
 promRouter.get(
-  '/prom',
+  '/cpu-usage',
   Promcontroller.cpuUsageByContainer,
   async (_, res) => {
     // console.log('res.locals.cluster', res.locals.cluster);
-    return res.status(200).json(res.locals.response);
+    return res.status(200).json(res.locals.cpuUsage);
   },
 );
 
