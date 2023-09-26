d071dbc7f4cb779e146e6d24364e238ea408b7e6
Nathan Gonsalves <iamkaprekar@gmail.com>
2023-09-26 16:28:35 -0400

    Frontend: added a modal to create new PromURL link. Backened: Created middleware to handle adding new PromURL link to database for current signed in user.

--- postgres_create.sql
+++ postgres_create.sql
@@ -11,20 +11,10 @@ CREATE TABLE public.users (
 CREATE TABLE public.clusters (
   "cluster_id" serial NOT NULL,
   "user_id" serial NOT NULL REFERENCES public.users(user_id),
+  "prom_url" text NOT NULL,
   CONSTRAINT "clusters_pk" PRIMARY KEY ("cluster_id")
 ) WITH (
   OIDS=FALSE
 );
 
-CREATE TABLE public.cpu_logs (
-  "cpu_log_id" serial NOT NULL,
-  "cluster_id" serial NOT NULL REFERENCES public.clusters(cluster_id),
-  "timestamp" timestamp NOT NULL,
-  "data" real NOT NULL,
-  CONSTRAINT "cpu_log_pk" PRIMARY KEY ("cpu_log_id")
-) WITH (
-  OIDS=FALSE
-);
-
 ALTER TABLE public.clusters ADD CONSTRAINT "users_fk0" FOREIGN KEY ("user_id") REFERENCES public.users("user_id");
-ALTER TABLE public.cpu_logs ADD CONSTRAINT "clusters_fk0" FOREIGN KEY ("cluster_id") REFERENCES public.clusters("cluster_id");
